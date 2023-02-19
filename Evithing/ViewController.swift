db9f8577a5edfb4b44d0f06fbbe53a1731dbe9ec
FarukAs <omeraso1998@gmail.com>
2023-02-19 19:53:39 +0300

    Initial Commit

---
+++ Evithing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Evithing
+//
+//  Created by Şeyda Soylu on 19.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
